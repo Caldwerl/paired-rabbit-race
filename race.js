db7a909b237574e68f05de4c5c71df31312117a2
Philip Nguyen <supertaru@gmail.com>
2014-08-07 14:48:48 -0700

    Change user inputs to grab from HTML forms

--- race.js
+++ race.js
@@ -7,6 +7,7 @@ var turn = 0;
 var finishLine = false;
 
 
+
 function Animal(name, speed, focus) {
   this.name = name;
   this.speed = speed;
@@ -28,46 +29,54 @@ function Animal(name, speed, focus) {
 //rabbit = new Animal("rabbit", 6, 4);
 //turtle = new Animal("turtle", 3, 9);
 
-// =========User Input of rabbit and turtle's attributes ==========
-endDistance = prompt("Please input the length of the race");
-inputRabbitName = prompt("Please input the name of the rabbit");
-inputRabbitSpeed = prompt("Please input the speed of rabbit the rabbit.");
-inputRabbitFocus = prompt("Please input the focus of rabbit the rabbit.");
-inputTurtleName = prompt("Please input the name of the turtle");
-inputTurtleSpeed = prompt("Please input the speed of turtle the turtle.");
-inputTurtleFocus = prompt("Please input the focus of turtle the turtle.");
-rabbit = new Animal(inputRabbitName, parseInt(inputRabbitSpeed), parseInt(inputRabbitFocus));
-turtle = new Animal(inputTurtleName, parseInt(inputTurtleSpeed), parseInt(inputTurtleFocus));
+var racerInput = function() {
+  // =========User Input of rabbit and turtle's attributes ==========
+  endDistance = document.getElementById('distance').value;
+  inputRabbitName = document.getElementById('quadOneName').value;
+  inputRabbitSpeed = document.getElementById('quadOneName').value;
+  inputRabbitFocus = document.getElementById('quadOneFocus').value;
+  inputTurtleName = document.getElementById('quadTwoName').value;
+  inputTurtleSpeed = document.getElementById('quadTwoSpeed').value;
+  inputTurtleFocus = document.getElementById('quadTwoFocus').value;
+  rabbit = new Animal(inputRabbitName, parseInt(inputRabbitSpeed), parseInt(inputRabbitFocus));
+  turtle = new Animal(inputTurtleName, parseInt(inputTurtleSpeed), parseInt(inputTurtleFocus));
+}
 
-confirmation = confirm("The distance to the goal line is 100 miles. Rabbit and Turtle, are set! Start simulation?");
+var startGame = function() {
+  racerInput();
 
-//checks for user confirmation
-if (confirmation == true) {
+  confirmation = confirm("The distance to the goal line is 100 miles. Rabbit and Turtle, are set! Start simulation?");
 
-  // if rabbit and turtle are not at finishline, continue to next turn
-  while((finishLine == false) && (confirmation == true)) {
-    distraction = Math.random() * 10;
+  //checks for user confirmation
+  if (confirmation == true) {
 
-    turn += 1;
+    // if rabbit and turtle are not at finishline, continue to next turn
+    while((finishLine == false) && (confirmation == true)) {
+      distraction = Math.random() * 10;
 
-    confirmation = confirm("Turn " + turn + "\n\n"
-      + rabbit.movement(distraction)
-      + "\n\n"
-      + turtle.movement(distraction) + "\n\nContinue?")
+      turn += 1;
 
-    //finish game if rabbit crosses the finishLine
-    if (rabbit.currentMiles >= endDistance) {
-      alert(rabbit.name + " won!");
-      finishLine = true;
-    }
+      confirmation = confirm("Turn " + turn + "\n\n"
+        + rabbit.movement(distraction)
+        + "\n\n"
+        + turtle.movement(distraction) + "\n\nContinue?")
 
-    //finish game if turtle crosses the finishline.
-    else if (turtle.currentMiles >= endDistance) {
-      alert(turtle.name + " won!");
-      finishLine = true;
+      //finish game if rabbit crosses the finishLine
+      if (rabbit.currentMiles >= endDistance) {
+        alert(rabbit.name + " won!");
+        finishLine = true;
+      }
+
+      //finish game if turtle crosses the finishline.
+      else if (turtle.currentMiles >= endDistance) {
+        alert(turtle.name + " won!");
+        finishLine = true;
+      };
     };
-  };
+  }
+  else {
+    alert("Goodbye!")
+  }
 }
-else {
-  alert("Goodbye!")
-}
+
+
